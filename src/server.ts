a42a43e8be29fb6780a9b7b175f321773b458fbc
Andrii Andreichenko <87246480+ShrewdLuni@users.noreply.github.com>
2025-10-17 15:22:52 +0200

    Refactor: relocated db related endpoints

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,7 @@
 import express from "express";
 import router from "routes";
-import { query } from "db";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import { migrate } from "migration";
 
 const app = express();
 const port = 3000;
@@ -13,24 +11,6 @@ app.use(cookieParser());
 app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 
 app.use("/api", router);
-
-app.get("/db/test", async (req, res) => {
-  const result = await query("SELECT NOW()");
-  if (result == undefined || result == null) {
-    res.status(500).send("PostgreSQL is not working");
-  }
-  res
-    .status(200)
-    .send(`PostgreSQL is online, server time is: ${result.rows[0].now}`);
-});
-
-app.get("/db/migrate", async (req, res) => {
-  var result = await migrate();
-  if (result == undefined || result == null || result == false) {
-    res.status(500).send("Migration failed");
-  }
-  res.status(200).send("Migration succeed");
-});
 
 app.listen(port, () => {
   console.log(`Listening on port: ${port}`);
