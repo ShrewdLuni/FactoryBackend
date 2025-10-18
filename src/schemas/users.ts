718db419aab747c1f63d7b8081b7e7c7f0db8264
Andrii Andreichenko <87246480+ShrewdLuni@users.noreply.github.com>
2025-10-18 10:52:56 +0200

    Updated user schema

---
+++ src/schemas/users.ts
@@ -0,0 +1,30 @@
+import { z } from "zod"
+
+export const userSchema = z.object({
+  id: z.number().int(),
+  code: z.number().int().optional(),
+  username: z.string().optional(),
+  firstName: z.string(),
+  lastName: z.string(),
+  patronymic: z.string().optional(),
+  fullName: z.string(),
+  dateOfBirth: z.coerce.date().optional(),
+  email: z.email().optional(),
+  phone: z.string().optional(),
+  gender: z.enum(["Male", "Female", "Other"]).optional(), 
+  department: z.string().optional(), 
+});
+
+export const insertUserSchema = userSchema.omit({ id: true, fullName: true })
+
+export type User = z.infer<typeof userSchema>;
+export type InsertUser = z.infer<typeof insertUserSchema>;
+
+export const registerSchema = insertUserSchema
+  .extend({ password: z.string().min(8) })
+  .transform(({ password, ...user }) => ({ user, password }));
+
+export const loginSchema = insertUserSchema
+  .pick({ code: true, username: true })
+  .extend({ password: z.string().min(8) })
+  .transform(({ password, ...user}) => ({ user, password }))
