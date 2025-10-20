b6707512597d49428ed6dc5bb2048e03e71f5aa4
Andrii Andreichenko <87246480+ShrewdLuni@users.noreply.github.com>
2025-10-20 11:31:28 +0200

    Feat: add auth middleware

---
+++ src/middleware/auth.ts
@@ -0,0 +1,23 @@
+import express from 'express'
+import jwt, { type JwtPayload } from 'jsonwebtoken'
+
+
+export const authenticate = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+  try {
+    const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
+    
+    if (!token) {
+      return res.sendStatus(401);
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET!) as JwtPayload
+
+    req.userId = decoded.userId;
+
+    next()
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(401);
+  }
+};
+
