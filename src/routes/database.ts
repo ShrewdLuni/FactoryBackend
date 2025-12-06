ac7bda79fdd1619608e1919cbdf77ddc8b4ca396
Andrii Andreichenko <87246480+ShrewdLuni@users.noreply.github.com>
2025-12-06 08:26:40 +0100

    chore: seed gate

--- src/routes/database.ts
+++ src/routes/database.ts
@@ -1,9 +1,21 @@
 import express from "express"
 import { migrateDatabase, testDatabase } from "controllers/database";
+import { query } from "db";
+import logger from "logger";
 
 const router = express.Router()
 
 router.get("/test", testDatabase)
 router.get("/migrate", migrateDatabase)
+router.get("/seed", async (req: express.Request, res: express.Response) => {
+  try {
+    await query(`INSERT INTO products (code, name, category) VALUES(152, 'Red Socks', 'Socks'), (164, 'Blue Socks', 'Socks'), (82, 'Xmas Socks', 'Socks(Special)')`);
+    logger.info("Seed succeeded");
+    res.sendStatus(200);
+  } catch(error) {
+    logger.error(error);
+    res.sendStatus(500);
+  }
+})
 
 export default router;
--- src/server.ts
+++ src/server.ts
@@ -10,9 +10,11 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
-app.use(logging)
+app.use(logging);
 
 app.use("/api", router);
+
+app.get("test", async (req: express.Request, res:express.Response) => {return res.status(200).json({"message": "Server is up"})});
 
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
