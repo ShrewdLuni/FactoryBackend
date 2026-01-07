7e5eb6ac7614050116fb41e41db6143b447981a6
Andrii Andreichenko <87246480+ShrewdLuni@users.noreply.github.com>
2026-01-07 13:42:05 +0100

    feat: 1C users WIP

--- src/routes/database.ts
+++ src/routes/database.ts
@@ -2,6 +2,7 @@ import express from "express"
 import { migrateDatabase, testDatabase } from "controllers/database";
 import { query } from "db";
 import logger from "logger";
+import { AddUsersToDB } from "utils/1C/AddUsersToDB";
 
 const router = express.Router()
 
@@ -10,11 +11,30 @@ router.get("/migrate", migrateDatabase)
 router.get("/seed", async (req: express.Request, res: express.Response) => {
   try {
     await query(`INSERT INTO products (code, name, category) VALUES(152, 'Red Socks', 'Socks'), (164, 'Blue Socks', 'Socks'), (82, 'Xmas Socks', 'Socks(Special)')`);
+    await query(`INSERT INTO users (username, first_name, last_name, gender, role) VALUES
+      ('master_one', 'Ivan', 'Petrov', 'Male', 'Master'),
+      ('master_two', 'Olga', 'Sidorova', 'Female', 'Master'),
+      ('master_three', 'Dmitry', 'Kuznetsov', 'Male', 'Master'),
+      ('master_four', 'Anastasia', 'Volkova', 'Female', 'Master'),
+      ('master_five', 'Sergey', 'Novikov', 'Male', 'Master'),
+      ('master_six', 'Dmitry', 'Volkov', 'Male', 'Worker'),
+      ('master_seven', 'Sergey', 'Brin', 'Male', 'Manager')`)
     logger.info("Seed succeeded");
     res.sendStatus(200);
   } catch(error) {
     logger.error(error);
     res.sendStatus(500);
+  }
+})
+router.get("/add-users", async (req: express.Request, res: express.Response) => {
+  try {
+    await AddUsersToDB()
+    logger.info("1C users added");
+    res.sendStatus(200);
+  }
+  catch {
+    logger.info("1C users fail");
+    res.sendStatus(200);
   }
 })
 
