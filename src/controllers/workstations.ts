dabbc3b758ea40cbbaedde8af1045d221389131a
Andrii Andreichenko <87246480+ShrewdLuni@users.noreply.github.com>
2026-01-09 19:16:26 +0100

    feat: CRUD workstations

--- src/controllers/workstations.ts
+++ src/controllers/workstations.ts
@@ -1,11 +1,29 @@
 import express from "express"
 import { InsertWorkstationSchema, WokrstationFromDatabase, WorkstationsFromDatabase } from "schemas/workstations"
-import { createWorkstation, getAllWorkstations } from "services/workstations"
+import { createWorkstation, deleteWorkstation, getAllWorkstations, getWorkstation, updateWorkstation } from "services/workstations"
 
 export const getAllWorkstationsController = async (req: express.Request, res: express.Response) => {
   const databaseResult = await getAllWorkstations()
   const workstations = WorkstationsFromDatabase.parse(databaseResult)
   return res.status(200).json(workstations);
+}
+
+export const getWorkstationController = async (req: express.Request, res: express.Response) => {
+  try {
+    if (req.params.id == undefined) {
+      return res.status(400).json({message: "Invalid data was provided"})
+    }
+    const id = parseInt(req.params.id);
+    if (isNaN(id)) {
+      return res.status(400).json({message: "Invalid data was provided"})
+    }
+    const result = await getWorkstation(id)
+    const workstation = WokrstationFromDatabase.parse(result); 
+    return res.status(200).json(workstation);
+  } catch (error) {
+    console.log(error);
+    return res.status(500);
+  }
 }
 
 export const createWorkstationController = async (req: express.Request, res: express.Response) => {
@@ -19,3 +37,41 @@ export const createWorkstationController = async (req: express.Request, res: exp
     return res.status(500);
   }
 }
+
+export const updateWorkstationController = async (req: express.Request, res: express.Response) => {
+  try {
+    if (req.params.id == undefined) {
+      return res.status(400).json({message: "Invalid data was provided"})
+    }
+    const id = parseInt(req.params.id);
+    if (isNaN(id)) {
+      return res.status(400).json({message: "Invalid data was provided"})
+    }
+    const data = InsertWorkstationSchema.parse(req.body); 
+    const result = await updateWorkstation(id, data)
+    const workstation = WokrstationFromDatabase.parse(result); 
+    return res.status(200).json(workstation);
+  } catch (error) {
+    console.log(error);
+    return res.status(500);
+  }
+}
+
+export const deleteWorkstationController = async (req: express.Request, res: express.Response) => {
+  try {
+    if (req.params.id == undefined) {
+      return res.status(400).json({message: "Invalid data was provided"})
+    }
+    const id = parseInt(req.params.id);
+    if (isNaN(id)) {
+      return res.status(400).json({message: "Invalid data was provided"})
+    }
+    const result = await deleteWorkstation(id)
+    const workstation = WokrstationFromDatabase.parse(result); 
+    return res.status(200).json(workstation);
+  } catch (error) {
+    console.log(error);
+    return res.status(500);
+  }
+}
+
--- src/services/workstations.ts
+++ src/services/workstations.ts
@@ -2,11 +2,26 @@ import type { DatabaseWorkstation, InsertWorkstation } from "schemas/workstation
 import { query } from "../db"
 
 export const getAllWorkstations = async (): Promise<DatabaseWorkstation[]> => {
-  const result = await query("SELECT * FROM workstations")
+  const result = await query("SELECT * FROM workstations");
   return result.rows
 }
 
+export const getWorkstation = async (id: number): Promise<DatabaseWorkstation> => {
+  const result = await query("SELECT * FROM workstations WHERE id = $1", [id]);
+  return result.rows[0];
+}
+
+export const updateWorkstation = async (id: number, data: InsertWorkstation): Promise<DatabaseWorkstation> => {
+  const result = await query("UPDATE workstations SET name = $2, qr_code = $3 WHERE id = $1 RETURNING *", [id, data.name, data.qr_code]);
+  return result.rows[0];
+}
+
+export const deleteWorkstation = async (id: number): Promise<DatabaseWorkstation> => {
+  const result = await query("DELETE FROM workstations WHERE id = $1 RETURNING *", [id]);
+  return result.rows[0];
+}
+
 export const createWorkstation = async (data: InsertWorkstation): Promise<DatabaseWorkstation> => {
-  const result = await query("INSERT INTO workstations (name, qr_code) VALUES($1, $2) RETURNING *", [data.name, data.qr_code])
+  const result = await query("INSERT INTO workstations (name, qr_code) VALUES($1, $2) RETURNING *", [data.name, data.qr_code]);
   return result.rows[0];
 }
