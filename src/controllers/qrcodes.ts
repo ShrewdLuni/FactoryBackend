2f55e1b31e6bc40a618fbb8c07fb08841caed068
Andrii Andreichenko <87246480+ShrewdLuni@users.noreply.github.com>
2026-02-13 10:14:01 +0100

    feat: CRUD qrcodes + refactor

--- src/controllers/qrcodes.ts
+++ src/controllers/qrcodes.ts
@@ -1,129 +1,74 @@
-import express from "express"
-import { InitializeQRCodeSchema, QRCodeFromDatabase, QRCodesFromDatabase } from "schemas/qrcode"
-import { createQRCodes, getQRCodes, activateQRCode, getQRCodeById } from "services/qrCodes"
+import express from "express";
+import { InitializeQRCodeSchema, InsertQRCodeSchema, QRCodeFromDatabase, QRCodesFromDatabase } from "schemas/qrcode";
+import { createQRCodes, getQRCodes, activateQRCode, getQRCode, deleteQRCode, updateQRCode } from "services/qrCodes";
 import QRCode from 'qrcode';
 import { BASE_URL, CLIENT_URL } from "config";
+import { asyncHandler, HttpError } from "utils/errorHandler";
+import { paramsSchema } from "schemas/utils";
 
-export const getQRCodesController = async (req: express.Request, res: express.Response) => {
-  try {
-    const data = await getQRCodes();
-    const qrCodes = QRCodesFromDatabase.parse(data);
+export const getQRCodesController = asyncHandler(async (req: express.Request, res: express.Response) => {
+  const databaseResult = await getQRCodes();
+  const qrCodes = QRCodesFromDatabase.parse(databaseResult);
+  const qrCodesWithImages = await Promise.all(
+    qrCodes.map(async (qr) => {
+      const scanUrl = `${BASE_URL}/api/qrcodes/${qr.id}/scan`;
+      const qrCodeDataUrl = await QRCode.toDataURL(scanUrl, {width: 300, margin: 2, errorCorrectionLevel: 'M'});
+      return {
+        ...qr,
+        qrcodeImage: qrCodeDataUrl,
+        scanUrl: scanUrl
+      };
+    })
+  );
+  res.status(200).json(qrCodesWithImages);
+})
 
-    const qrCodesWithImages = await Promise.all(
-      qrCodes.map(async (qr) => {
-        const scanUrl = `${BASE_URL}/api/qrcodes/${qr.id}/scan`;
-        const qrCodeDataUrl = await QRCode.toDataURL(scanUrl, {
-          width: 300,
-          margin: 2,
-          errorCorrectionLevel: 'M'
-        });
-        
-        return {
-          ...qr,
-          qrcodeImage: qrCodeDataUrl,
-          scanUrl: scanUrl
-        };
-      })
-    );
-    return res.status(200).json(qrCodesWithImages)
-  } catch (error) {
-    console.log(error)
-    return res.status(500);
-  }
-}
+export const getQRCodeController = asyncHandler(async (req: express.Request, res: express.Response) => {
+  const { id } = paramsSchema.parse(req.body);
+  const result = await getQRCode(id);
+  if (!result) throw new HttpError(404, `QR-Code with ID ${id} not found`);
+  const qrCode = QRCodeFromDatabase.parse(result);
+  res.status(200).json(qrCode);
+})
 
-export const getQRCodeController = async (req: express.Request, res: express.Response) => {
-  try {
-    if (req.params.id === undefined) {
-      return res.status(400).json({message: "Invalid data was provided"})
-    }
+export const createQRCodesController = asyncHandler(async (req: express.Request, res: express.Response) => {
+  const { qrcode, amount } = InitializeQRCodeSchema.parse(req.body); 
+  const data = await createQRCodes(qrcode, amount);
+  const result = QRCodesFromDatabase.parse(data);
+  res.status(201).json(result);
+})
 
-    const id = parseInt(req.params.id)
+export const updateWorkstationController = asyncHandler(async (req: express.Request, res: express.Response) => {
+  const { id } = paramsSchema.parse(req.params)
+  const data = InsertQRCodeSchema.parse(req.body); 
+  const result = await updateQRCode(id, data)
+  const qrCode = QRCodeFromDatabase.parse(result); 
+  res.status(200).json(qrCode);
+})
 
-    if (isNaN(id)) {
-      return res.status(400).json({message: "Invalid data was provided"})
-    }
+export const deleteQRCodeController = asyncHandler(async (req: express.Request, res: express.Response) => {
+  const { id } = paramsSchema.parse(req.body);
+  const result = await deleteQRCode(id);
+  if (!result) throw new HttpError(404, `QR-Code with ID ${id} not found`);
+  const qrCode = QRCodeFromDatabase.parse(result);
+  res.status(200).json(qrCode);
+})
 
-    const data = await getQRCodeById(id)
-    const qrCode = QRCodeFromDatabase.parse(data)
-    return res.status(200).json(qrCode)
-  } catch (error) {
-    console.log(error)
-    return res.status(500);
-  }
-}
+export const activateQRCodeController = asyncHandler(async (req: express.Request, res: express.Response) => {
+  const { id } = paramsSchema.parse(req.body);
+  const resource = req.body.resource; 
+  if(resource == undefined) throw new HttpError(400, "Resource was not provided");
+  const data = await activateQRCode(id, resource);
+  if(!data) throw new HttpError(404, `QR-Code with ID ${id} not found`);
+  const result = QRCodeFromDatabase.parse(data);
+  res.status(200).json(result);
+})
 
-export const createQRCodesController = async (req: express.Request, res: express.Response) => {
-  try {
-    const { qrcode, amount } = InitializeQRCodeSchema.parse(req.body); 
-
-    const data = await createQRCodes(qrcode, amount)
-    const result = QRCodesFromDatabase.parse(data)
-
-    return res.status(200).json(result);
-  } catch (error) {
-    console.log(error)
-    return res.status(500);
-  }
-}
-
-export const activateQRCodeController = async (req: express.Request, res: express.Response) => {
-  try {
-    
-    if (req.params.id == undefined) {
-      return res.status(400).json({message: "Invalid data was provided"})
-    }
-
-    const id = parseInt(req.params.id);
-
-    if (isNaN(id)) {
-      return res.status(400).json({message: "Invalid data was provided"})
-    }
-
-    const resource = req.body.resource; 
-
-    if(resource == undefined) {
-      return res.status(400).json({message: "Invalid data was provided"})
-    }
-
-    const data = await activateQRCode(id, resource)
-    const result = QRCodeFromDatabase.parse(data)
-    return res.status(200).json(result);
-  } catch (error) {
-    console.log(error)
-    return res.status(500);
-  }
-
-}
-
-export const scanQRCodeController = async (req: express.Request, res: express.Response) => {
-  try {
-    if (req.params.id === undefined) {
-      return res.status(400).json({message: "Invalid data was provided"})
-    }
-
-    const id = parseInt(req.params.id)
-
-    if (isNaN(id)) {
-      return res.status(400).json({message: "Invalid data was provided"})
-    }
-
-    const result = await getQRCodeById(id);
-    const qrCode = QRCodeFromDatabase.parse(result);
-    
-    if (!qrCode) {
-      return res.status(404).send('QR Code not found');
-    }
-    
-    if (qrCode.resource) {
-      return res.redirect(qrCode.resource);
-    }
-
-    return res.redirect(`${CLIENT_URL}/qrcodes/${qrCode.id}`)
-    
-    return res.status(200).send(`QR Code "${qrCode.name}" - No resource linked yet`);
-  } catch (error) {
-    console.log(error);
-    return res.status(500).send('Error processing scan');
-  }
-}
+export const scanQRCodeController = asyncHandler(async (req: express.Request, res: express.Response) => {
+  const { id } = paramsSchema.parse(req.params);
+  const result = await getQRCode(id);
+  const qrCode = QRCodeFromDatabase.parse(result);
+  if(!qrCode) throw new HttpError(404, `QR-Code with ID ${id} not found`);
+  if (qrCode.resource) res.redirect(qrCode.resource);
+  res.redirect(`${CLIENT_URL}/qrcodes/${qrCode.id}`)
+})
--- src/services/qrCodes.ts
+++ src/services/qrCodes.ts
@@ -1,18 +1,28 @@
 import { query } from "db";
 import type { DatabaseQRCode, InsertQRCode } from "schemas/qrcode";
 
-export const createQRCodes = async (data: InsertQRCode, amount: number): Promise<DatabaseQRCode[]> => {
-  const result = await query(`INSERT INTO qr_codes (name, resource) SELECT $1, $2 FROM generate_series(1, $3) RETURNING *`, [data.name ?? null, data.resource ?? null, amount])
-  return result.rows;
-};
-
 export const getQRCodes = async (): Promise<DatabaseQRCode[]> => {
   const result = await query(`SELECT * FROM qr_codes`);
   return result.rows;
 };
 
-export const getQRCodeById = async (id: number): Promise<DatabaseQRCode[]> => {
+export const getQRCode = async (id: number): Promise<DatabaseQRCode[]> => {
   const result = await query(`SELECT * FROM qr_codes WHERE id = $1`, [id]);
+  return result.rows[0];
+}
+
+export const createQRCodes = async (data: InsertQRCode, amount: number): Promise<DatabaseQRCode[]> => {
+  const result = await query(`INSERT INTO qr_codes (name, resource) SELECT $1, $2 FROM generate_series(1, $3) RETURNING *`, [data.name, data.resource, amount])
+  return result.rows;
+};
+
+export const updateQRCode = async (id: number, data: InsertQRCode): Promise<DatabaseQRCode> => {
+  const result = await query(`UPDATE qr_codes SET name = $2, resource = $3 WHERE id = $1 RETURNING *`, [id, data.name, data.resource])
+  return result.rows[0];
+}
+
+export const deleteQRCode = async (id: number): Promise<DatabaseQRCode[]> => {
+  const result = await query(`DELETE FROM qr_codes WHERE id = $1`, [id]);
   return result.rows[0];
 }
 
