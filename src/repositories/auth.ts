a8add3a32f81256e4b9c3e3f7270eb5b5bf6d9f0
Andrii Andreichenko <87246480+ShrewdLuni@users.noreply.github.com>
2026-03-06 17:49:06 +0100

    feat: auth repository

---
+++ src/repositories/auth.ts
@@ -0,0 +1,31 @@
+
+import { query } from "db";
+import { type Authentication, AuthenticationFromRow, type AuthenticationInsert, type AuthenticationRow } from "schemas/authentication";
+
+export class AuthenticationRepository {
+  async find(id: number): Promise<Authentication> {
+    const result = await query<AuthenticationRow>(`SELECT * FROM authentication WHERE id = $1 LIMIT 1`, [id]);
+    const rows = result.rows;
+    return AuthenticationFromRow.parse(rows[0]);
+  }
+  
+  async create(data: AuthenticationInsert): Promise<Authentication> {
+    const result = await query<AuthenticationRow>(`INSERT INTO authentication (user_id, hash, salt) VALUES($1, $2, $3) RETURNING *`, [data.userId, data.hash, data.salt]);
+    const rows = result.rows;
+    return AuthenticationFromRow.parse(rows[0]);
+  }
+
+  async update(id: number, data: AuthenticationInsert): Promise<Authentication> {
+    const result = await query<AuthenticationRow>(`UPDATE authentication SET hash = $2, salt = $3 WHERE user_id = $1 RETURNING *`, [id, data.hash, data.salt])
+    const rows = result.rows;
+    return AuthenticationFromRow.parse(rows[0]);
+  }
+
+  async delete(id: number): Promise<Authentication> {
+    const result = await query<AuthenticationRow>(`DELETE FROM authentication WHERE id = $1 RETURNING *`, [id]);
+    const rows = result.rows;
+    return AuthenticationFromRow.parse(rows[0]);
+  }
+}
+
+
--- src/schemas/authentication.ts
+++ src/schemas/authentication.ts
@@ -10,31 +10,23 @@ const mapped = {
   userId: DbId
 }
 
-const AuthenticationSchema = z.object({...shared, ...mapped})
+export const AuthenticationSchema = z.object({...shared, ...mapped})
 
-const DatabaseAuthenticationSchema = z.object({
+export const AuthenticationRowSchema = z.object({
   ...shared,
   user_id: mapped.userId,
 })
 
-export const DatabaseFromAuthentication = AuthenticationSchema.transform((auth) => {
-  const { userId, ...rest } = auth;
+export const AuthenticationFromRow = AuthenticationRowSchema.transform((row) => {
+  const { user_id, ...rest } = row;
   return ({
-    ...rest,
-    user_id: userId,
-  })
-})
-
-export const AuthenticationFromDatabase = DatabaseAuthenticationSchema.transform((db) => {
-  const { user_id, ...rest } = db;
-  return ({
-    ...rest,
     userId: user_id,
+    ...rest,
   })
 })
 
-export const InsertAuthenticationSchema = AuthenticationSchema.omit({})
+export const AuthenticationInsertSchema = AuthenticationSchema.omit({})
 
 export type Authentication = z.infer<typeof AuthenticationSchema>;
-export type DatabaseAuthentication = z.infer<typeof DatabaseAuthenticationSchema>;
-export type InsertAuthentication = z.infer<typeof AuthenticationSchema>;
+export type AuthenticationRow = z.infer<typeof AuthenticationRowSchema>;
+export type AuthenticationInsert = z.infer<typeof AuthenticationInsertSchema>;
