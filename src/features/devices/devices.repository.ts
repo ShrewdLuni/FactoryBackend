6ba1b0256fa5d5234e553ec9a8fc90d94106191b
Andrii Andreichenko <87246480+shrewddd@users.noreply.github.com>
2026-06-26 00:38:16 +0200

    refactor: device findMany

--- src/features/devices/devices.repository.ts
+++ src/features/devices/devices.repository.ts
@@ -1,8 +1,19 @@
 import { Repository } from "abstract/repository";
 import { DeviceFromRow, type Device, type DeviceInsert, type DeviceLookup, type DeviceRow } from "./devices.schema";
+import { query } from "db";
 
 export class DeviceRepository extends Repository<Device, DeviceRow, DeviceLookup, DeviceInsert> {
   constructor() {
     super("devices", DeviceFromRow, { name: "name", capacity: "capacity", isActive: "is_active", department: { column: 'department_id', extract: (d) => d.department.id  } })
+  }
+
+  async findMany(): Promise<Device[]> {
+    const result = await query<DeviceRow>(`SELECT 
+      dev.*, 
+      dep.label AS department_label
+      FROM devices dev LEFT JOIN departments dep
+      ON dev.department_id = dep.id
+    `);
+    return DeviceFromRow.array().parse(result.rows);
   }
 }
