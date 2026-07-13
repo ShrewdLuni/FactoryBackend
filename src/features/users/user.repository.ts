298731270533bc7c3494403df8fd153516fcf9d0
Andrii Andreichenko <87246480+shrewddd@users.noreply.github.com>
2026-07-13 10:37:03 +0200

    feat: user department sync, PROTO, WILL BE FIXED later, hopefully

--- src/features/users/user.repository.ts
+++ src/features/users/user.repository.ts
@@ -1,12 +1,13 @@
 import { Repository } from "abstract/repository";
 import { UserFromRow, type User, type UserInsert, type UserLookup, type UserRow } from "./user.schema";
+import { query } from "db";
 
 export class UserRepository extends Repository<User, UserRow, UserLookup, UserInsert> {
   constructor() {
     super("Users", UserFromRow, {
       firstName: "first_name",
       lastName: "last_name",
-      departmentIds: "departments",
+      // departmentIds: "departments",
       email: "email",
       isActive: "is_active",
       code: "code",
@@ -15,7 +16,16 @@ export class UserRepository extends Repository<User, UserRow, UserLookup, UserIn
       phone: "phone",
       gender: "gender",
       dateOfBirth: "date_of_birth",
-      role: { column: "role_id", extract: (d) => d.role.id }
+      role: { column: "role_id", extract: (d) => d.role.id },
     });
+  }
+  async syncDepartments(id: number, departmentIds: number[]): Promise<void> {
+    console.log(departmentIds, "crazy here")
+    await query(`DELETE FROM user_departments WHERE user_id = $1`, [id]);
+    if (departmentIds.length === 0) return;
+    const values = departmentIds.map((_, i) => `($1, $${i + 2})`).join(", ");
+    await query(
+    `INSERT INTO user_departments (user_id, department_id) VALUES ${values}`,
+    [id, ...departmentIds])
   }
 }
--- src/features/users/user.service.ts
+++ src/features/users/user.service.ts
@@ -1,9 +1,24 @@
 import { Service } from "abstract/service";
 import { UserRepository } from "./user.repository";
 import type { User, UserInsert, UserLookup } from "./user.schema";
+import { DepartmentRepository } from "features/departments/department.repository";
+import { query, transaction } from "db";
 
 export class UserService extends Service<User, UserInsert, UserLookup, UserRepository> {
-  constructor(repo: UserRepository = new UserRepository()){
+  private departmentRepository: DepartmentRepository;
+
+  constructor(repo: UserRepository = new UserRepository(), departmentRepository: DepartmentRepository = new DepartmentRepository()){
     super(repo)
+    this.departmentRepository =  departmentRepository;
+  }
+
+  async patch(id: number, data: Partial<UserInsert>): Promise<User> {
+    const result = await transaction(async (client) => {
+      const user = await super.patch(id, data)
+      if (data.departmentIds !== undefined &&  data.departmentIds !== null) 
+        await this.repository.syncDepartments(id, data.departmentIds);
+      return user;
+    })
+    return result;
   }
 }
